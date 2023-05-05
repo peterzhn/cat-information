b59310b88bec6ce968552f4493a243eb74c1bbf0
peterzhn <peterzhn@gmail.com>
2023-05-05 19:14:54 -0400

    v3

--- main.js
+++ main.js
@@ -31,4 +31,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(process.env.PORT, () => console.log('Server running on port 3000'));
