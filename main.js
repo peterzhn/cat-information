ea7630f142920035ef7e689d0c324962f63208da
peterzhn <peterzhn@gmail.com>
2023-05-05 19:04:11 -0400

    v2

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ const catsData = require('./cats.json');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { "Access-Control-Allow-Origin": "*", "Content-Type": "text/html" });
 
     // read the index.html file and send it as response
     const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
     res.write(html);
     res.end();
   } else if (req.url === '/cats') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, { "Access-Control-Allow-Origin": "*", "Content-Type": "application/json" });
+
 
     // get a random cat from the catsData array
-    const randomCat = catsData[Math.floor(Math.random() * catsData.length)];
+    const randomCat = catsData.cats[Math.floor(Math.random() * catsData.cats.length)];
 
     // send the random cat as JSON response
     res.write(JSON.stringify(randomCat));
